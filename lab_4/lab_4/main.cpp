0faf87ad68ea26fea15aca73324486bc228a53d2
Quitianote <santiago.quitian@udea.edu.co>
2023-04-29 12:19:05 -0500

    al fin encontre un algoritmo que sirva

--- lab_4/lab_4/main.cpp
+++ lab_4/lab_4/main.cpp
@@ -2,49 +2,53 @@
 #include <fstream>
 #include <vector>
 #include <map>
+#include <string>
+
+#define INF 999
 
 using namespace std;
 
+void printMatrix(int matrix[][99]);
+
 int main(){
     ifstream archivo_nodos;
-    map <char, vector<char>> nodos;
+    string reng;
 
+    map <string, vector<string>> nodos;
+    map <string, int> info;
     archivo_nodos.open("nodos.txt");
+
+    while(archivo_nodos.good()){
+        archivo_nodos.getline(reng, 256);
+    }
 
     cout << "Hello World!" << endl;
     return 0;
 }
 
-void leernodo(char reng[], map <string, int>& mapa, char codi[]){//leer renglon de nodos
+void leernodo(string reng,map <string, vector<vector<string>>> &mapa, map <string, int> &info){//leer renglon de nodos, es necesario el ampersan para editar directamente el mapa, eso encontre
     int pos = 0;
     int cont = 4;
     bool aux = false;
+    string nom = "";
+    string cone[256] ;
 
     for(int i = 0; i < 256; i ++){
-        if(reng[i] == '*'){//codigo
-            for(int a = i + 1; a < 256; a ++){//creando codigo
-                if(reng[a] != '!'){
-                    mat[0][pos] = reng[a];//posicion en la matriz
-                    //cout << mat[0] << " codigo" << endl;
-                    char temp1 = mat[0][pos];
-                    char temp2 = codi[pos];
-                    if(temp1 != temp2) return false;
-                    pos ++;
-
+        if(reng[i] == "*"){//nombre de nodo
+            for(int a = i + 1; a < 256; a ++){//nombre de nodo
+                if(reng[a] != ","){
+                    nom.append(reng[a]);//armando nombre
                 }
                 else break;
             }
         }
-        else if(reng[i] == '!'){
+        else if(reng[i] == ","){//conexiones
             pos = 0;
-            for(int a = i + 1; a < 256; a ++){//creando nombre de materia
-                if(reng[a] != '#'){
-                    //cout << mat[1] << " nombre1" << endl;
-                    //cout << reng[a] << " letra de renglon" << endl;
-                    mat[1][pos] = reng[a];
-                   // cout << a << " a" << endl;
-                   // cout << mat[1] << " nombre" << endl;
-                    pos ++;//posicion en la matriz
+            for(int a = i + 1; a < 256; a ++){//creando conexiones
+                if(reng[a] != "," && reng[a] != "-"){
+                    if(reng[a] == "/") cone.append("INF")
+                    cone.append(reng[a]);
+
                 }
                 else{
                     mat[1][pos] = '\0';
@@ -83,3 +87,53 @@ void leernodo(char reng[], map <string, int>& mapa, char codi[]){//leer renglon 
     }
     return true;
 }
+
+//FLOYD ALGORITMO
+
+
+
+
+
+
+void floydWarshall(int graph[][99]) {
+  int nV = 4;
+  int matrix[99][99], i, j, k;
+
+  for (i = 0; i < nV; i++)
+    for (j = 0; j < nV; j++)
+      matrix[i][j] = graph[i][j];
+
+  // Adding vertices individually
+  for (k = 0; k < nV; k++) {
+    for (i = 0; i < nV; i++) {
+      for (j = 0; j < nV; j++) {
+        if (matrix[i][k] + matrix[k][j] < matrix[i][j])
+          matrix[i][j] = matrix[i][k] + matrix[k][j];
+      }
+    }
+  }
+  printMatrix(matrix);
+}
+
+void printMatrix(int matrix[][99]) {
+  int nV = 4;
+  for (int i = 0; i < nV; i++) {
+    for (int j = 0; j < nV; j++) {
+      if (matrix[i][j] == INF)
+        printf("%4s", "INF");
+      else
+        printf("%4d", matrix[i][j]);
+    }
+    printf("\n");
+  }
+}
+/*
+int main() {
+  int nV = 4;
+  int graph[99][99] = {{0, 5, 3, INF},
+             {5, 0, 1, INF},
+             {3, 1, 0, 7},
+             {INF, INF, 7, 0}};
+  floydWarshall(graph);
+}
+*/
